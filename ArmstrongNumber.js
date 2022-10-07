53e288058432a0f887ba674bf9eba3aa69e44476
Nanda-Krishna-Hub <74372675+Nanda-Krishna-Hub@users.noreply.github.com>
2022-10-07 23:22:31 +0530

    Add files via upload

---
+++ ArmstrongNumber.js
@@ -0,0 +1,15 @@
+let n = 417;
+let number = n;
+let digit = 0;
+let result = 0;
+while(n>0){
+    digit = n % 10;
+    n = parseInt(n/10);
+    result += (digit*digit*digit);
+}
+console.log(result)
+if(number == result){
+    console.log(`${number} is Armstrong number`);
+}
+else
+console.log(`${number} is not Armstrong number`);
